712a61d234e92f94fbcfad9ffd39108d10fb9c95
275313730 <a27531730@qq.com>
2022-02-18 00:30:23 +0800

    完善功能

--- src/plugins/card/__init__.py
+++ src/plugins/card/__init__.py
@@ -6,10 +6,17 @@ from nonebot.adapters.onebot.v11 import Bot, Event, MessageSegment
 # from . import card_pre
 from . import card_get
 from . import image
+from . import crew_battle
 
 roles = on_command("全部式神", aliases={"所有式神"}, priority=2, block=True)
 
 role_cards = on_command("式神", priority=2, block=True)
+
+set_battle_day = on_command("比赛日期", priority=3, block=True)
+
+sign_up = on_command("参加比赛", aliases={"报名比赛，报名参赛"}, priority=3, block=True)
+
+check_battle_stats = on_command("查看比赛", priority=3, block=True)
 
 
 @roles.handle()
@@ -28,3 +35,27 @@ async def _(bot: Bot, event: Event):
             await role_cards.send(MessageSegment(type="image", data={"file": file_base64}))
     else:
         await role_cards.send("式神名字呢？")
+
+
+@set_battle_day.handle()
+async def _(bot: Bot, event: Event):
+    args = str(event.get_message()).split(" ")
+    if len(args) > 1:
+        crew_battle.set_battle_day(args[1])
+        await set_battle_day.send(f"比赛日期已经设定为{args[1]}（注意修改比赛日期会重置报名信息）")
+    else:
+        await set_battle_day.send("比赛日期呢？")
+
+
+@sign_up.handle()
+async def _(bot: Bot, event: Event):
+    args = str(event.get_message()).split(" ")
+    if len(args) > 1:
+        await sign_up.send(crew_battle.sign_up(args[1]))
+    else:
+        await sign_up.send("虚空参赛是吧？")
+
+
+@check_battle_stats.handle()
+async def _(bot: Bot, event: Event):
+    await check_battle_stats.send(crew_battle.check_battle_stats())
--- src/plugins/public/__init__.py
+++ src/plugins/public/__init__.py
@@ -8,4 +8,8 @@ _help = on_command("help", aliases={"帮助"}, priority=1, block=True)
 
 @_help.handle()
 async def _(bot: Bot, event: Event):
-    await _help.send("目前功能:\n/全部式神or/所有式神：查看所有式神的名称\n/式神 xx：查看指定式神的卡牌（例如：/式神 不知火）")
+    await _help.send(f"目前功能:\n/全部式神 or /所有式神：查看所有式神的名称"
+                     f"\n/式神 xx：查看指定式神的卡牌（例如：/式神 不知火）"
+                     f"\n/比赛日期 xx：设置比赛时间（注意每次设置会重置比赛信息）"
+                     f"\n/参加比赛 xx or /报名比赛 xx or /报名参赛 xx：报名参加比赛"
+                     f"\n/查看比赛：获取比赛信息")
