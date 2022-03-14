c2c61c9609eeec6b78f0bd122280e1a143d7cb68
275313730 <a275313730@qq.com>
2022-03-14 16:26:30 +0800

    将检测功能移至命令预处理函数中，避免代码重复

--- src/plugins/tree_hole/matcher/note.py
+++ src/plugins/tree_hole/matcher/note.py
@@ -9,17 +9,6 @@ from ..handle import user, note
 add_note = on_command("投递小纸条", rule=to_me(), priority=2, block=True)
 
 
-@add_note.handle()
-async def _(event: PrivateMessageEvent):
-    qq = event.user_id
-    exist = user.check_qq_exist(qq)
-    ban = user.check_qq_ban(qq)
-    if not exist:
-        await add_note.finish("加入树洞才能投递小纸条哦")
-    if ban:
-        await add_note.finish("账号封禁中，如有疑问可以通过'/反馈树洞'提交意见")
-
-
 @add_note.got("content", prompt="随便写点什么都可以哦（目前只支持纯文字消息，请勿发送表情和图片等其他内容）")
 async def _(state: T_State, event: PrivateMessageEvent):
     qq = event.user_id
@@ -41,26 +30,14 @@ async def _(event: PrivateMessageEvent):
     qq = event.user_id
 
     user.update_last_use_time(qq)
-    exist = user.check_qq_exist(qq)
-    if exist:
-        note_str = note.get_random_note(qq)
-        if note_str != "":
-            await random_note.finish(note_str)
-        else:
-            await random_note.finish("暂无小纸条")
+    note_str = note.get_random_note(qq)
+    if note_str != "":
+        await random_note.finish(note_str)
     else:
-        await random_note.finish("加入树洞才能看到别人的小纸条哦")
+        await random_note.finish("暂无小纸条")
 
 
 my_notes = on_command("我的小纸条", rule=to_me(), priority=2, block=True)
-
-
-@my_notes.handle()
-async def _(event: PrivateMessageEvent):
-    qq = event.user_id
-    exist = user.check_qq_exist(qq)
-    if not exist:
-        await random_note.finish("你还没有加入树洞呢")
 
 
 @my_notes.handle()
@@ -76,14 +53,6 @@ async def _(event: PrivateMessageEvent):
 
 
 delete_note = on_command("删除小纸条", rule=to_me(), priority=2, block=True)
-
-
-@delete_note.handle()
-async def _(event: PrivateMessageEvent):
-    qq = event.user_id
-    exist = user.check_qq_exist(qq)
-    if not exist:
-        await delete_note.finish("你还没有加入树洞呢")
 
 
 @delete_note.got("uid", prompt="请输入小纸条编号")
@@ -109,13 +78,6 @@ async def _(state: T_State, event: PrivateMessageEvent):
 
 
 report_note = on_command("举报小纸条", rule=to_me(), priority=2, block=True)
-
-
-@report_note.handle()
-async def _(event: PrivateMessageEvent):
-    exist = user.check_qq_exist(event.user_id)
-    if not exist:
-        await report_note.finish("请加入树洞后再使用其他命令")
 
 
 @report_note.got("uid", prompt="请输入小纸条编号")
