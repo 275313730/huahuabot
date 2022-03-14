bf352ffc6a90ca98f5c3ae477e1e80de4f85ae26
275313730 <a275313730@qq.com>
2022-03-14 16:25:46 +0800

    命令预处理函数中增加忽略命令的功能

--- src/plugins/tree_hole/matcher/hook.py
+++ src/plugins/tree_hole/matcher/hook.py
@@ -1,12 +1,21 @@
+from nonebot.dependencies import Dependent
 from nonebot.internal.matcher import Matcher
 from nonebot.adapters.onebot.v11 import PrivateMessageEvent
 from nonebot.message import run_preprocessor
+from nonebot.rule import CommandRule
 
 from ..handle import user
+
+ignore_cmds = ['help', '帮助', '加入树洞']
 
 
 @run_preprocessor
 async def check_ban(matcher: Matcher, event: PrivateMessageEvent):
+    checker: Dependent = list(matcher.rule.checkers)[0]
+    call: CommandRule = checker.call
+    for cmd in call.cmds:
+        if cmd[0] in ignore_cmds:
+            return
     qq = event.user_id
     exist = user.check_qq_exist(qq)
     ban = user.check_qq_ban(qq)
