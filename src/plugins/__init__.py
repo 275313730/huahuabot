375dfa82746b00308eb3bdf1aa236fa443dfd0be
275313730 <a27531730@qq.com>
2022-03-24 22:54:59 +0800

    好友自动通过

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -10,8 +10,7 @@ friend_req = on_request(priority=5)
 
 @friend_req.handle()
 async def friend_agree(bot: Bot, event: FriendRequestEvent):
-    if str(event.user_id) in bot.config.superusers:
-        await bot.set_friend_add_request(flag=event.flag, approve=True)
+    await bot.set_friend_add_request(flag=event.flag, approve=True)
 
 bot_help = on_command("help", aliases={"帮助"}, priority=2, block=True)
 
